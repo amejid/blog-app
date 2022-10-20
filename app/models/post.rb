6dc9ddc7db0b8a3c42dd45107292e5fe6de7e875
amejid <abdelmejid89@gmail.com>
2022-10-20 14:11:57 +0300

    Implement a method for recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  belongs_to :author, class_name: 'User'
+
+  def return_recent_comments
+    comment.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
