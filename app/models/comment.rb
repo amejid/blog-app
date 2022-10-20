7cff47cc9c5058d018beea5f5debf7083f68d11a
amejid <abdelmejid89@gmail.com>
2022-10-20 14:12:39 +0300

    Update comments counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  private
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
