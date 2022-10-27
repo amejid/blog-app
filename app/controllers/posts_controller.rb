d77dedf72644cbfb97d13a9904466624fe8cee2c
amejid <abdelmejid89@gmail.com>
2022-10-27 16:08:23 +0300

    Create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,27 @@
 class PostsController < ApplicationController
+  before_action :set_user, only: [:index, :create, :show]
   def index
-    set_user
     @posts = @user.posts
   end
 
   def show
     set_post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @post = Post.new
   end
 
   private
@@ -16,5 +32,9 @@ class PostsController < ApplicationController
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
