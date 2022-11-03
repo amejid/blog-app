efc704c4455c230d515b5d74fa84136650587da5
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-03 14:40:24 +0100

    Fix routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show] do
-    # resources :posts, except: [:update, :edit] do
-    #   resources :comments, only: [:new, :create, :destroy]
+    resources :posts, except: [:update, :edit] do
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
