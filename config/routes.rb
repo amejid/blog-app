4297b9a689dcc191aeaddbaef40762a156be0368
amejid <abdelmejid89@gmail.com>
2022-11-04 19:20:33 +0300

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api do
+    namespace :v1 do
+      post :auth, to: 'authentication#create'
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
+
   resources :users, only: [:index, :show] do
     resources :posts, except: [:update, :edit] do
       resources :comments, only: [:new, :create, :destroy]
